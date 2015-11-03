2589ac8a58f04ac952febdb0aade731eedc04d46
spreddy2714 <spreddy2714@gmail.com>
2015-11-03 10:25:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,28 +12,16 @@
  * Main module for components
  */
 
- angular.module('sas-ux.components.common',[]);
- angular.module('sas-ux.components.attachmentpicker',[]);
- angular.module('sas-ux.components.localstorage', []);
- angular.module('sas-ux.components.spinner',[]);
- angular.module('sas-ux.components.confirmation',[]);
- angular.module('sas-ux.components.customize',[]);
- angular.module('sas-ux.components.daterangepicker',[]);
- angular.module('sas-ux.components.fileupload',[]);
- angular.module('sas-ux.components.relink',[]);
 
  angular.module('sas-ux.components', [
-    'sas-ux.components.common',
     'sas-ux.components.attachmentpicker',
     'sas-ux.components.confirmation',
-    'sas-ux.components.customize',
     'sas-ux.components.daterangepicker',
     'sas-ux.components.fileupload',
 	'sas-ux.components.localstorage',
 	'sas-ux.components.oauth',
 	'sas-ux.components.relink',
 	'sas-ux.components.spinner',
-	'sas-ux.components.security',
 	'sas-ux.components.sidebar'
 	]);
 })();
